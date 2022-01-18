46d6faee2eb3adba677320170c5b188f6510bd7b
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-01-18 13:10:34 -0500

    update to tokencheck mwerror handling

--- src/server/middleware/tokenCheck.mw.ts
+++ src/server/middleware/tokenCheck.mw.ts
@@ -1,6 +1,6 @@
 import * as passport from 'passport';
-import {Request, Response, NextFunction} from 'express';
-import {ReqUser} from '../types';
+import { Request, Response, NextFunction } from 'express';
+import { ReqUser } from '../types';
 
 export function tokenCheck(req: ReqUser, res: Response, next: NextFunction) {
 
@@ -11,31 +11,34 @@ export function tokenCheck(req: ReqUser, res: Response, next: NextFunction) {
             return next(err)
         }
 
-        if (info){
+        if (info) {
             return res.status(401).json({
                 message: 'Error while authenticating, please log in again',
                 error: info.message
             });
         }
 
-        if(!user) {
+        if (!user) {
 
-            return res.redirect('/login')
+            return res.status(404).json({
+                error: 'Error: User not found'
+            })
+
 
         }
 
 
-        if(user) {
+        if (user) {
 
             console.log('INSIDE TOKENCHECK if(user) block:');
             delete user.password;
-            
-            
+
+
             console.log('Token is good!');
             req.user = user;
         }
         next();
 
-    })(req, res, next)  
+    })(req, res, next)
 
 };
